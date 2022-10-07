5f9112bab6be66e5f0f7ad6e855bd4824b3ecb5c
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-10-07 23:26:08 +0900

    Add files via upload

---
+++ square.c
@@ -0,0 +1,31 @@
+#include <GL/glut.h>
+#include <GL/GL.h>
+#include <GL/GLU.h>
+
+void MyDisplay() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glViewport(0, 0, 300, 300);
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_POLYGON);
+		glVertex3f(-0.5, -0.5, 0.0);
+		glVertex3f(0.5, -0.5, 0.0);
+		glVertex3f(0.5, 0.5, 0.0);
+		glVertex3f(-0.5, 0.5, 0.0);
+	glEnd();
+	glFlush();
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB);
+	glutInitWindowSize(300, 300);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("OpenGL Sample Drawing");
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+	glutDisplayFunc(MyDisplay);
+	glutMainLoop();
+	return 0;
+}
